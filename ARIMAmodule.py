b17b841b9392512da0a9a457e7bf58e1c23c331c
fengkuang-yu <34153864+fengkuang-yu@users.noreply.github.com>
2018-10-26 22:44:09 +0800

    完成代码校验

--- ARIMAmodule.py
+++ ARIMAmodule.py
@@ -13,16 +13,42 @@ import itertools
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import scipy.stats as scs
 import matplotlib.pyplot as plt
+import statsmodels.tsa.api as smt
 from arch import arch_model
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+
+def tsplot(y, lags=None, figsize=(10, 8), style='bmh'):
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+    with plt.style.context(style):
+        fig = plt.figure(figsize=figsize)
+        # mpl.rcParams['font.family'] = 'Ubuntu Mono'
+        layout = (3, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        qq_ax = plt.subplot2grid(layout, (2, 0))
+        pp_ax = plt.subplot2grid(layout, (2, 1))
+
+        y.plot(ax=ts_ax)
+        ts_ax.set_title('Time Series Analysis Plots')
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax, alpha=0.5)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=0.5)
+        sm.qqplot(y, line='s', ax=qq_ax)
+        qq_ax.set_title('QQ Plot')
+        scs.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
+
+        plt.tight_layout()
+    return
 
 # 处理输入数据
 plt.style.use('fivethirtyeight')
 pd_data = pd.read_csv(r'D:\Users\yyh\Pycharm_workspace\hybrid_model\Data\flow_data_59.csv')
 y = pd.Series(pd_data['20.93'])
 y.index = pd.date_range(start='2016-02-01 00:00:00', periods=16992,freq='5min',normalize=True)
-
 
 # 对输入数据进行历史平均处理,这里构造三个特征：
 # 1. 第一个特征（history_average）是历史平均值，使用历史平均值作为周期/季节成分
@@ -65,28 +91,30 @@ plt.show()
 
 
 # 使用AIC进行模型选择
-p = d = q = range(0, 3)
-pdq = list(itertools.product(p, d, q))
-warnings.filterwarnings("ignore")  # 忽略参数配置时的警告信息
-for param in pdq:
-    try:
-        mod = sm.tsa.statespace.SARIMAX(deterministic, order=param)
-        results = mod.fit()
-        print('ARIMA{}- AIC:{}'.format(param, results.aic))
-    except:
-        continue
+# p = d = q = range(0, 3)
+# pdq = list(itertools.product(p, d, q))
+# warnings.filterwarnings("ignore")  # 忽略参数配置时的警告信息
+# for param in pdq:
+#     try:
+#         mod = sm.tsa.statespace.SARIMAX(deterministic, order=param)
+#         results = mod.fit()
+#         print('ARIMA{}- AIC:{}'.format(param, results.aic))
+#     except:
+#         continue
 
 # 建立ARIMA模型，最优参数根据AIC准则选取（2, 0，2）
 mod = sm.tsa.statespace.SARIMAX(deterministic,
                                 order=(2, 0, 2),
                                 enforce_stationarity=False,
                                 enforce_invertibility=False)
-results = mod.fit(disp=0)
-print(results.summary().tables[1])  # 模型的诊断表
+results = mod.fit()
+print(results.summary())  # 模型的诊断表
 results.plot_diagnostics(figsize=(15, 12))  # 模型的拟合诊断图
 plt.show()
 residuals = pd.DataFrame(results.resid)  # 对于训练数据的拟合的残差值
 residuals = residuals.rename(columns={0:'Residuals'})  # 改变列的名字
+smoothed_deterministic = results.fittedvalues  # deterministic中取出residuals的剩余值
+# y == results.fittedvalues + results.resid + history_average
 
 # 画出拟合残差的图像并进行ACF和PACF分析
 residuals.plot()
@@ -95,106 +123,72 @@ plt.xlabel('Date', fontsize=14)
 plt.show()
 print(residuals.describe())
 # ARIMA的残差
-residuals = results.forecasts_error
-plot_acf(residuals.flatten(), lags=576)
+plot_acf(residuals, lags=30)
 plt.title('')
 plt.ylabel('Residuals ACF', fontsize=16)
 plt.xlabel('Time Lag(5mins)', fontsize=16)
 plt.show()
-plot_pacf(residuals.flatten(), lags=576)
+plot_pacf(residuals, lags=30)
 plt.title('')
 plt.ylabel('Residuals PACF', fontsize=16)
 plt.xlabel('Time Lag(5mins)', fontsize=16)
 plt.show()
 
+# 利用建立好的模型进行预测
+pred_test = results.forecast()
+pred = results.get_prediction(start=pd.to_datetime('2016-02-01 00:00:00'), dynamic=False)
 
-pred = results.get_prediction(start=pd.to_datetime('2016-02-01 00:00:00'), dynamic=False)  # 利用建立好的模型进行预测
-pred_ci = pred.conf_int()  # 置信度区间计算
-
-ax = y['2016-02-01 00:00:00':].plot(label='observed')  # 画出真实值
-pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7)  # 画出预测的值
-ax.fill_between(pred_ci.index,  # 画出置信区间，其中pred_ci.iloc[:,0]表示的是下界，另外一个表示的是上界
-                pred_ci.iloc[:, 0],  # 下界
-                pred_ci.iloc[:, 1],  # 上界
-                color='k',
-                alpha=.2)
-ax.set_xlabel('Date')
-ax.set_ylabel('traffic flow')
-plt.legend()
-plt.show()
-y_forecasted = pred.predicted_mean
-y_truth = deterministic['2016-02-01 00:00:00':]
-
-# 预测结果的ARIMA的残差分析
-residuals = results.forecasts_error
-plot_acf(residuals.flatten(), lags=576)
-plt.title('')
-plt.ylabel('Residuals ACF', fontsize=16)
-plt.xlabel('Time Lag(5mins)', fontsize=16)
-plt.show()
-plot_pacf(residuals.flatten(), lags=576)
-plt.title('')
-plt.ylabel('Residuals PACF', fontsize=16)
-plt.xlabel('Time Lag(5mins)', fontsize=16)
-plt.show()
+# pred_ci = pred.conf_int()  # 置信度区间计算
+# # 画出真实值和预测值的对比图
+# ax = y['2016-02-01 00:00:00':].plot(label='observed')  # 画出真实值
+# pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7)  # 画出预测的值
+# ax.fill_between(pred_ci.index,  # 画出置信区间，其中pred_ci.iloc[:,0]表示的是下界，另外一个表示的是上界
+#                 pred_ci.iloc[:, 0],  # 下界
+#                 pred_ci.iloc[:, 1],  # 上界
+#                 color='k',
+#                 alpha=.2)
+# ax.set_xlabel('Date')
+# ax.set_ylabel('traffic flow')
+# plt.legend()
+# plt.show()
 
 # 计算预测误差
-mae = ((y_forecasted - y_truth) / y_truth).abs().mean()
-print('The Mean Absolute Error of our forecasts is {}'.format(round(mae, 5)))
-
-# ARIMA的残差
-residuals = results.forecasts_error
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-plot_acf(residuals.flatten(), lags=10)
-plt.show()
-plot_pacf(residuals.flatten(), lags=10)
-plt.show()
+arima_forecasted = pred.predicted_mean + history_average
+y_truth = y['2016-02-01 00:00:00':]
+mae = ((arima_forecasted[2:] - y_truth[2:]) / y_truth[2:]).abs().mean()
+print('The Mean Absolute Error of ARIMA forecasts is {}'.format(round(mae, 5)))
 
 # 检验序列具有ARCH效应
-at = residuals.flatten()[0:2880]
+at = residuals[0:2880]
 at2 = np.square(at)
-plt.figure(figsize=(10,6))
-plt.subplot(211)
-plt.plot(at,label = 'at')
-plt.legend()
-plt.subplot(212)
-plt.plot(at2,label='at^2')
-plt.legend(loc=0)
-plt.show()
-# 检查残差平方项的ACF和PACF效应
-plot_acf(at2, lags=30)
-plt.title('', fontsize=16)
-plt.ylabel('Squared Value of Residuals ACF', fontsize=16)
-plt.xlabel('Time Lag(5mins)', fontsize=16)
-plt.show()
-plot_pacf(at2, lags=30)
-plt.title('', fontsize=16)
-plt.ylabel('Squared Value of Residuals PACF', fontsize=16)
-plt.xlabel('Time Lag(5mins)', fontsize=16)
+fig = plt.figure(figsize=(10,6))
+layout = (2, 2)
+at2_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+acf_ax = plt.subplot2grid(layout, (1, 0))
+pacf_ax = plt.subplot2grid(layout, (1, 1))
+at2.plot(ax=at2_ax)
+at2_ax.legend_.remove()
+at2_ax.xaxis.set_tick_params(rotation=0, labelsize=10)
+at2_ax.set_title('Squared Residuals')
+at2_ax.set_xlabel('Date', fontsize=16)
+smt.graphics.plot_acf(at2, lags=30, ax=acf_ax, alpha=0.5)
+acf_ax.set_xlabel('Time Lag(5mins)', fontsize=16)
+smt.graphics.plot_pacf(at2, lags=30, ax=pacf_ax, alpha=0.5)
+pacf_ax.set_xlabel('Time Lag(5mins)', fontsize=16)
+plt.tight_layout()
 plt.show()
 
 # 建立GJR-ARCH模型
-returns = pd.Series(residuals.flatten(),
-                    index=pd.date_range(start='2016-02-01 00:00:00',
-                                        periods=16992,
-                                        freq='5min',
-                                        normalize=True
-                                        )
-                    )
-am = arch_model(returns, vol='Garch', p=1, q=1, dist='normal')
+am = arch_model(residuals, vol='Garch', p=1, q=1, dist='StudentsT')
 res = am.fit(update_freq=5, disp='off')
 print(res.summary())
-
-index = returns.index
+index = residuals.index
 start_loc = 0
-end_loc = 20
+end_loc = 288
 forecasts = {}
-import sys
-for i in range(len(returns)-20):
-    sys.stdout.write('.')
-    sys.stdout.flush()
+for i in range(len(residuals)-288):
     res2 = am.fit(first_obs=i, last_obs=i+end_loc, disp='off')
-    temp = res.forecast(horizon=1).variance
+    temp = res2.forecast(horizon=1).variance
     fcast = temp.iloc[i+end_loc-1]
     forecasts[fcast.name] = fcast
 
@@ -205,17 +199,6 @@ plt.show()
 res.conditional_volatility.plot(label='volatility_real')
 plt.legend()
 plt.show()
-
-
-# 原始序列构造完整序列处理过程与原始序列进行对比
-# hybird_data = smoothed_data + res.resid
-# mae2 = np.mean(np.abs(((hybird_data - real_data) / real_data)[1:]))
-# print('The Mean Absolute Error of our forecasts is {}'.format(round(mae2, 5)))
-#
-# plt.plot(hybird_data[288:576], label='hybrid_data', linewidth=1)
-# plt.plot(real_data[288:576], label='real_data', linewidth=1)
-# plt.show()
-# pd.Series(res.conditional_volatility).to_csv(r"D:\桌面\res_conditional_volatility.csv")
 
 
 mae = (np.abs(np.array(history_average) - np.array(y_truth))/np.array(y_truth)).mean()
